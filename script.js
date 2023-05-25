921b7aad85132478552799f6c0fb717f0da4013c
Nikolai <nikolai2002b@gmail.com>
2023-05-26 01:24:14 +0200

    Hotfix fix auth not working due to import/export

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-"use strict";
-import { calculateMembershipFee } from "/pay.js";
-import { getTotalMembershipFee } from "/pay.js";
-import { getAllResults } from "/pay.js";
-import { getUserId } from "/api.js";
-import { getByTournament } from "/pay.js";
-import { getTopSwimmersByDiscipline } from "/pay.js";
-import { displayTopSwimmers } from "/pay.js";
-import { getProfiles } from "/api.js";
-
 const endpoint =
   "https://javascriptgame-4e4c9-default-rtdb.europe-west1.firebasedatabase.app";
 
@@ -24,6 +14,591 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
+
+async function deleteData(id, type) {
+  // delete item globally
+  const url = `${endpoint}/${type}/${id}.json`;
+  const response = await fetch(url, { method: "DELETE" });
+  if (response.ok) {
+    //delete locally
+    document.querySelector(`#${id}`).remove();
+    //show user message
+    response_message("SUCCESS! DATA SLETTET");
+  } else if (!response.ok) {
+    response_message("ERROR: DATA IKKE SLETTET");
+  }
+}
+//getting a specific user with the uid from auth
+
+async function getUserId() {
+  try {
+    const user = firebase.auth().currentUser;
+    const uid = user.uid;
+
+    const endpoint =
+      "https://javascriptgame-4e4c9-default-rtdb.europe-west1.firebasedatabase.app";
+    const response = await fetch(`${endpoint}/users/${uid}.json`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+
+    const userData = await response.json();
+    const id = Object.keys(userData)[0];
+
+    return id;
+  } catch (error) {
+    console.error("Error fetching user ID:", error);
+    throw error;
+  }
+}
+//smart way of getting all ids inside user => uid => (id)
+async function getAllUserIds(token) {
+  try {
+    const endpoint =
+      "https://javascriptgame-4e4c9-default-rtdb.europe-west1.firebasedatabase.app/users.json";
+    const response = await fetch(`${endpoint}?auth=${token}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error fetching user IDs.");
+    }
+
+    const usersData = await response.json();
+    const userIds = Object.keys(usersData).map((userId) => usersData[userId]);
+
+    const userData = userIds.filter((user) => typeof user === "object");
+
+    return userData.map((user) => Object.values(user)[0]);
+  } catch (error) {
+    console.error("Error fetching user IDs:", error);
+    throw error;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const userDataForm = document.getElementById("userDataForm");
+
+  userDataForm.addEventListener("submit", async function (e) {
+    //console.log("works");
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("age").value;
+    const userType = document.getElementById("userType").value;
+
+    const gender = document.getElementById("gender").value;
+    const phone = document.getElementById("phone").value;
+    //const payed = document.getElementById("payed").checked;
+
+    const user = firebase.auth().currentUser;
+    const uid = user.uid;
+
+    const userData = {
+      name: name,
+      age: age,
+      userType: userType,
+
+      gender: gender,
+      phone: phone,
+      //payed: payed,
+    };
+    //PATCH private data inside uid/id
+    const id = await getUserId();
+    console.log(await getUserId());
+    const endpoint = `https://javascriptgame-4e4c9-default-rtdb.europe-west1.firebasedatabase.app/users/${uid}/${id}.json`;
+
+    fetch(endpoint, {
+      method: "PATCH",
+      body: JSON.stringify(userData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("User data updated successfully");
+        } else {
+          throw new Error("Failed to update user data");
+        }
+      })
+      .catch((error) => {
+        console.error("Error updating user data:", error);
+      });
+  });
+});
+
+async function displayUserData() {
+  try {
+    const user = firebase.auth().currentUser;
+    const uid = user.uid;
+    const id = await getUserId();
+    //console.log("works");
+
+    const endpoint =
+      "https://javascriptgame-4e4c9-default-rtdb.europe-west1.firebasedatabase.app/users";
+
+    const response = await fetch(`${endpoint}/${uid}/${id}.json`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+
+    const userData = await response.json();
+    console.log(userData.email);
+    //the form fields with the user data
+    const age = document.getElementById("age");
+
+    const nameInput = document.getElementById("name");
+    const phoneInput = document.getElementById("phone");
+    const userTypeSelect = document.getElementById("userType");
+    const genderSelect = document.getElementById("gender");
+
+    //set the form field values
+
+    age.value = userData.age || "0";
+    nameInput.value = userData.name || "0";
+    phoneInput.value = userData.phone || "0";
+    userTypeSelect.value = userData.userType || "0";
+    genderSelect.value = userData.gender || "0";
+  } catch (error) {
+    console.error("Error displaying user data:", error);
+    throw error;
+  }
+}
+
+//getting all profiles with auth token
+async function getProfiles(token, uid) {
+  console.log(token);
+  const endpoint =
+    "https://javascriptgame-4e4c9-default-rtdb.europe-west1.firebasedatabase.app/users";
+  const response = await fetch(`${endpoint}.json?auth=${token}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Error fetching user profiles.");
+  }
+  const users = await getAllUserIds(token);
+  console.log("works", users);
+  const users1 = await response.json();
+
+  const tableContainer = document.getElementById("table-container");
+
+  //clear existing table content
+  tableContainer.innerHTML = "";
+
+  //create new table
+  const table = document.createElement("table");
+  table.classList.add("user-profiles");
+
+  //create table header
+  const tableHeader = document.createElement("thead");
+  const headerRow = document.createElement("tr");
+  const headers = [
+    "Navn",
+    "Alder",
+    "Email",
+    "Køn",
+    "Gruppe",
+    "Abonnement",
+    "Betalingsdato",
+    "Abonnementspris",
+    "Restance",
+    "Opdater bruger",
+  ];
+
+  headers.forEach((headerText) => {
+    const headerCell = document.createElement("th");
+    headerCell.textContent = headerText;
+    headerCell.classList.add("header-cell"); // Add the CSS class
+    headerRow.appendChild(headerCell);
+  });
+
+  tableHeader.appendChild(headerRow);
+  table.appendChild(tableHeader);
+
+  //create the table body
+  const tableBody = document.createElement("tbody");
+
+  Object.entries(users).forEach(([userId, user]) => {
+    const userRow = document.createElement("tr");
+
+    //name
+    const nameCell = document.createElement("td");
+    nameCell.textContent = user.name;
+    nameCell.classList.add("table-cell");
+    userRow.appendChild(nameCell);
+
+    //age
+    const ageCell = document.createElement("td");
+    ageCell.textContent = user.age;
+    ageCell.classList.add("table-cell");
+    userRow.appendChild(ageCell);
+
+    //email
+    const emailCell = document.createElement("td");
+    emailCell.textContent = user.email;
+    emailCell.classList.add("table-cell");
+    userRow.appendChild(emailCell);
+
+    //gender
+    const genderCell = document.createElement("td");
+    genderCell.textContent = user.gender;
+    genderCell.classList.add("table-cell");
+    userRow.appendChild(genderCell);
+
+    //stage
+    const stageCell = document.createElement("td");
+    stageCell.textContent = user.stage;
+    stageCell.classList.add("table-cell");
+    userRow.appendChild(stageCell);
+
+    //subscription
+    const subscriptionCell = document.createElement("td");
+    subscriptionCell.textContent = user.subscription;
+    subscriptionCell.classList.add("table-cell");
+    userRow.appendChild(subscriptionCell);
+
+    //billingdateCell
+    const billingdateCell = document.createElement("td");
+    billingdateCell.textContent = user.billingdate;
+    billingdateCell.classList.add("table-cell");
+    userRow.appendChild(billingdateCell);
+
+    //membershipFee
+    const membershipFee = document.createElement("td");
+    membershipFee.textContent = user.membershipFee;
+    membershipFee.classList.add("table-cell");
+    userRow.appendChild(membershipFee);
+
+    //payed
+    const payed = document.createElement("td");
+    payed.textContent = user.payed;
+    payed.classList.add("table-cell");
+    userRow.appendChild(payed);
+
+    //updatebutton
+    const updateCell = document.createElement("td");
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Slet bruger";
+
+    const updateButton = document.createElement("button");
+    updateButton.textContent = "Opdater data";
+    updateButton.addEventListener("click", () => {
+      updateButton.textContent = user.uid;
+    });
+    deleteButton.addEventListener("click", () => {
+      //update button click for the specific user in the table
+      console.log("userid", user);
+      deleteButton.textContent = user.uid;
+
+      //email is not null
+      if (user.email !== null) {
+        //confirmation popup
+        const confirmed = confirm(
+          "Er du sikker på at du vil slette denne bruger?"
+        );
+        //delete request til at slette bruger ud fra deres uid
+        if (confirmed) {
+          try {
+            const endpoint =
+              "https://javascriptgame-4e4c9-default-rtdb.europe-west1.firebasedatabase.app/users";
+            const response = fetch(`${endpoint}/${user.uid}.json`, {
+              method: "DELETE",
+            });
+
+            if (response.ok) {
+              console.log("User deleted successfully");
+            } else {
+              throw new Error("OK");
+            }
+          } catch (error) {
+            console.error("Error deleting user:", error);
+            //error message in a confirm view
+            window.alert("Bruger er nu tom men google auth er stadig aktiv");
+          }
+        }
+      } else {
+        console.log("Cannot delete user with null email");
+        //error message or handle the case when email is null
+        window.alert(
+          "Kan ikke slette allerede tom bruger eller bruger med høj status"
+        );
+      }
+      location.reload();
+    });
+
+    updateButton.classList.add("update-button");
+    updateCell.appendChild(updateButton);
+
+    deleteButton.classList.add("delete-button");
+    updateCell.appendChild(deleteButton);
+
+    userRow.appendChild(updateCell);
+
+    tableBody.appendChild(userRow);
+  });
+
+  table.appendChild(tableBody);
+  tableContainer.appendChild(table);
+}
+
+function calculateMembershipFee(age, active, ageGroup, uid, id) {
+  let membershipFee = 0;
+  //console.log(age, active, ageGroup);
+  if (active) {
+    if (ageGroup === "junior" && age < 18) {
+      membershipFee = 1000;
+    } else if (ageGroup === "senior" && age >= 18 && age < 60) {
+      membershipFee = 1600;
+    } else if (ageGroup === "senior" && age >= 60) {
+      //25% discount for members over 60
+      membershipFee = 1600 * 0.75;
+    }
+  } else {
+    //passive membership fee
+    membershipFee = 500;
+  }
+
+  const data = {
+    membershipFee: membershipFee,
+  };
+  //PATCH request to update profile data
+  fetch(`${endpoint}/users/${uid}/${id}.json`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Membership fee posted successfully:", data);
+    })
+    .catch((error) => {
+      console.error("Error posting membership fee:", error);
+    });
+
+  return membershipFee;
+}
+
+async function getTotalMembershipFee(token) {
+  try {
+    const response = await fetch(`${endpoint}/users.json?auth=${token}`);
+
+    const usersData = await response.json();
+
+    let totalMembershipFee = 0;
+
+    //loop each user and calculate the membership fees
+    for (const userId in usersData) {
+      const id = Object.keys(usersData[userId])[0];
+      const user = usersData[userId][id];
+
+      if (user != null) {
+        const membershipFee = user.membershipFee || 0;
+        //console.log(membershipFee);
+        totalMembershipFee += membershipFee;
+      }
+    }
+    //console.log("Total Membership Fee:", totalMembershipFee);
+    return totalMembershipFee;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return 0;
+  }
+}
+//get all results
+async function getAllResults(token) {
+  try {
+    const response = await fetch(`${endpoint}/results.json?auth=${token}`);
+
+    const usersResults = await response.json();
+    let totalMembershipFee = [];
+
+    for (const userId in usersResults) {
+      //const id = Object.keys(usersResults[userId])[0];
+
+      const user = usersResults[userId];
+
+      if (user != null) {
+        totalMembershipFee.push(user);
+      }
+    }
+
+    return totalMembershipFee;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return "error in getting user results";
+  }
+}
+//get by tournament will only get by senior og junior results
+async function getByTournament(token, traintype) {
+  try {
+    const response = await fetch(
+      `${endpoint}/results.json?auth=${token}&orderBy="traintype"&equalTo="${traintype}"`
+    );
+
+    console.log(response);
+    const usersResults = await response.json();
+    //const id = Object.keys(usersResults)[0];
+    //console.log("ID:", id);
+    const filteredResults = Object.values(usersResults).filter(
+      (result) => result.traintype.toLowerCase() === traintype.toLowerCase()
+    );
+    console.log(filteredResults);
+
+    return filteredResults;
+  } catch (error) {
+    console.error("Error fetching results:", error);
+    throw error;
+  }
+}
+
+async function getTopSwimmersByDiscipline(token) {
+  try {
+    const response = await fetch(`${endpoint}/results.json?auth=${token}`);
+    const usersResults = await response.json();
+
+    //object to store top swimmers by discipline and category
+    const topSwimmers = {
+      junior: {
+        butterfly: [],
+        crawl: [],
+        backstroke: [],
+        breaststroke: [],
+      },
+      senior: {
+        butterfly: [],
+        crawl: [],
+        backstroke: [],
+        breaststroke: [],
+      },
+      tournament: [],
+    };
+
+    //tterate over the users results and categorize them by discipline and category
+    Object.values(usersResults).forEach((result) => {
+      const {
+        swimmingdiscipline,
+        stage,
+        time,
+        name,
+        email,
+        tournament,
+        placement,
+      } = result;
+      const discipline = swimmingdiscipline.toLowerCase();
+      const category = stage;
+      const swimmer = { name, time, email, tournament, placement, stage };
+
+      if (topSwimmers[category] && topSwimmers[category][discipline]) {
+        topSwimmers[category][discipline].push(swimmer);
+      }
+
+      if (tournament !== null) {
+        topSwimmers.tournament.push(swimmer);
+      }
+    });
+
+    //sort the top swimmers by time in each discipline and category
+    Object.keys(topSwimmers).forEach((category) => {
+      if (category !== "tournament") {
+        Object.keys(topSwimmers[category]).forEach((discipline) => {
+          topSwimmers[category][discipline].sort(
+            (swimmer1, swimmer2) => swimmer1.time - swimmer2.time
+          );
+
+          //keeping only the top 5 swimmers
+          topSwimmers[category][discipline] = topSwimmers[category][
+            discipline
+          ].slice(0, 5);
+        });
+      }
+    });
+
+    console.log(topSwimmers);
+    return topSwimmers;
+  } catch (error) {
+    console.error("Error fetching results:", error);
+    throw error;
+  }
+}
+function displayTopSwimmers(topSwimmers) {
+  const juniorColumn = document.getElementById("junior-column");
+  const seniorColumn = document.getElementById("senior-column");
+  const tournamentColumn = document.getElementById("tournament-column");
+
+  // Clear the existing content in the columns
+  juniorColumn.innerHTML = "";
+  seniorColumn.innerHTML = "";
+  tournamentColumn.innerHTML = "";
+
+  // Top swimmers for each discipline in the junior category
+  Object.entries(topSwimmers.junior).forEach(([discipline, swimmers]) => {
+    const disciplineSection = document.createElement("div");
+    disciplineSection.classList.add("discipline-section");
+
+    const disciplineHeader = document.createElement("h3");
+    disciplineHeader.textContent = discipline.toUpperCase() + " Junior";
+    disciplineSection.appendChild(disciplineHeader);
+
+    swimmers.forEach((swimmer) => {
+      const swimmerElement = document.createElement("div");
+      swimmerElement.classList.add("swimmer");
+      swimmerElement.textContent = `Navn: ${swimmer.name}, Email: ${swimmer.email}, (${swimmer.time} Sekunder)`;
+      disciplineSection.appendChild(swimmerElement);
+    });
+
+    juniorColumn.appendChild(disciplineSection);
+  });
+
+  // Top swimmers for each discipline in the senior category
+  Object.entries(topSwimmers.senior).forEach(([discipline, swimmers]) => {
+    const disciplineSection = document.createElement("div");
+    disciplineSection.classList.add("discipline-section");
+
+    const disciplineHeader = document.createElement("h3");
+    disciplineHeader.textContent = discipline.toUpperCase() + " Senior";
+    disciplineSection.appendChild(disciplineHeader);
+
+    swimmers.forEach((swimmer) => {
+      const swimmerElement = document.createElement("div");
+      swimmerElement.classList.add("swimmer");
+      swimmerElement.textContent = `Navn: ${swimmer.name}, Email: ${swimmer.email}, (${swimmer.time} Sekunder)`;
+      disciplineSection.appendChild(swimmerElement);
+    });
+
+    seniorColumn.appendChild(disciplineSection);
+  });
+
+  // Swimmers with tournament information
+  const tournamentHeader = document.createElement("h2");
+  tournamentHeader.textContent = "Turneringsresultater";
+  tournamentColumn.appendChild(tournamentHeader);
+
+  const tournamentSwimmers = topSwimmers.tournament;
+  if (tournamentSwimmers) {
+    tournamentSwimmers.forEach((swimmer) => {
+      const swimmerElement = document.createElement("div");
+      swimmerElement.classList.add("swimmer");
+      swimmerElement.textContent = `Navn: ${swimmer.name}, Email: ${swimmer.email}, Gruppe: ${swimmer.stage},  (${swimmer.time} Sekunder)`;
+      swimmerElement.textContent += ` ${swimmer.tournament},  ${swimmer.placement}.`;
+      tournamentColumn.appendChild(swimmerElement);
+    });
+  }
+}
 
 var oceanheading = document.getElementById("welcome-text");
 oceanheading.style.display = "block";
